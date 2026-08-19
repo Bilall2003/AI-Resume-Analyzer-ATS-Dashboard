0a83d68feea1464e206c1dd1bdabb04dcf3d72ec
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-08-19 09:38:11 +0000

    update changes

--- HOME.py
+++ HOME.py
@@ -283,8 +283,9 @@ st.markdown("""
             
             """, unsafe_allow_html=True)
 
-if __name__ == "__main__":
-    
-    pass
 
+st.sidebar.title("Options")
+
+lst=["Home","Analyze_your_Resume","About"]
+st.sidebar.button(lst)
         
